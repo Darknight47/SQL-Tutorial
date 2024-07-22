7bc4dab1f9ca68511dce4473d8511c789a5556a3
Darknight47 <farhadgunners@gmail.com>
2024-07-22 14:13:36 +0200

    basic select

--- Select Queries/basic_select.sql
+++ Select Queries/basic_select.sql
@@ -1,10 +1,14 @@
 /* 
+The SELECT statement is used to select data from a database. The data returned is stored in a result table, called the result-set.
+
 -- Baic Syntax of SELECT QUERY
 -- SELECT columnName1, columnName2, ..., columnName(n)
 -- FROM table_name
+
+Here, column1, column2, ... are the field names of the table you want to select data from.
 */
 
--- Example 1: Select all columns from the 'customers' table
+-- Example 1: Select ALL columns from the 'customers' table
 SELECT *
 FROM customers;
 
